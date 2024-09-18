4400717d48844618204cc96c7ba588102a6273e5
38282010 <38282010@L815WD220014.edu.cbe.ab.ca>
2024-09-18 11:16:00 -0600

    11:15

--- Chapter3/src/SkillBuilder/RectangleP.java
+++ Chapter3/src/SkillBuilder/RectangleP.java
@@ -1,10 +1,35 @@
 package SkillBuilder;
 
+import java.util.Scanner;
+
 public class RectangleP {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+Scanner userInput = new Scanner(System.in);
 
+		//Declaration and initialization
+
+		System.out.print("Please enter a lenth value: ");
+
+		int length = userInput.nextInt();
+
+		
+
+		System.out.print("Please enter a width value: ");
+
+		int width = userInput.nextInt();
+
+
+
+		int area = length * width;
+
+				
+
+		System.out.print("The area of the rectangle is: "+ area);
+
+				
 	}
 
 }
