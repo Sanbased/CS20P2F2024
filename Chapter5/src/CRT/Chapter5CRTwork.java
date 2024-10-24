b053bbf14b1d350a874f534afb8470784fafa318
38282010 <38282010@L815WD220014.edu.cbe.ab.ca>
2024-10-24 10:25:45 -0600

    10:25

---
+++ Chapter5/src/CRT/Chapter5CRTwork.java
@@ -0,0 +1,15 @@
+package CRT;
+
+import java.util.Scanner;
+
+public class Chapter5CRTwork {
+	public static void main(String[] args) 
+	{
+
+		
+		 for (int i = 2; i <= 10; i++) {
+			int total = i;
+			System.out.println(total);
+					 }
+}
+}
