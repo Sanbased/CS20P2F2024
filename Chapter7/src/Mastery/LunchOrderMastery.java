061215ad703ac389b2dac82189066b88430bcea8
Sanje <Sanje@SanjeevPC.lan>
2024-12-04 20:41:48 -0700

    8:41

--- Chapter7/src/Mastery/LunchOrderMastery.java
+++ Chapter7/src/Mastery/LunchOrderMastery.java
@@ -7,41 +7,39 @@ public class LunchOrderMastery {
 
 	public static void main(String[] args) {
 		
+		int numham;
+		int numsalad;
+		int numff;
+		int numsoda;
+		
 		Scanner input = new Scanner(System.in);
 		
 		DecimalFormat formating = new DecimalFormat("#.##");
 		
-		FoodInfo foodorder = new FoodInfo();
+		FoodInfo hamburger = new FoodInfo(1.85, 33, 9, 1);
+		FoodInfo salad = new FoodInfo(2.00, 11, 1, 5);
+		FoodInfo ff = new FoodInfo(1.30, 36, 11, 4);
+		FoodInfo soda = new FoodInfo(0.95, 38, 0, 0);
 		
 		
 		System.out.println("Enter number of hamburgers: ");
-		
-		foodorder.hamburger(input.nextInt(), 9, 33, 1);
-		
-		System.out.println("");
-		
+		numham = input.nextInt();
+		System.out.println("Each hamburger has " + hamburger.GCarb() + "g of carbs, " + hamburger.GFat() + " g of fat, and " + hamburger.GFiber() + "g of fiber");
 		
 		System.out.println("Enter number of Salad: ");
-		
-		foodorder.salad(input.nextInt(), 1, 11, 5);
-		
-		System.out.println("");
-		
+		numsalad = input.nextInt();
+		System.out.println("Each salad has " + salad.GCarb() + "g of carbs, " + salad.GFat() + " g of fat, and " + salad.GFiber() + "g of fiber");		
 		
 		System.out.println("Enter number of French Fries: ");
-		
-		foodorder.FrenchFries(input.nextInt(), 11, 36, 4);
-		
-		System.out.println("");
-		
+		numff= input.nextInt();
+		System.out.println("Each order of French Fries has " + ff.GCarb() + "g of carbs, " + ff.GFat() + " g of fat, and " + ff.GFiber() + "g of fiber");		
 		
 		System.out.println("Enter number of Sodas: ");
-		
-		foodorder.Soda(input.nextInt(), 0, 38, 0);
-		
-		System.out.println("Your order comes to: $" + formating.format(foodorder.totalPrice()));
+		numsoda = input.nextInt();
+		System.out.println("Each soda has " + soda.GCarb() + "g of carbs, " + soda.GFat() + " g of fat, and " + soda.GFiber() + "g of fiber");		
 		
 	
+		System.out.println("Your order comes to: $" + formating.format(numham*hamburger.TPrice() + numsalad*salad.TPrice() + numff*ff.TPrice() + numsoda*soda.TPrice()));
 		
 	}
 }
