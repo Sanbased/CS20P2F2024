9b01495f2d2f0cde695da57505f9b40d57e31b3a
38282010 <38282010@L815WD220014.edu.cbe.ab.ca>
2024-12-12 10:04:52 -0700

    10

--- Chapter9/src/Mastery/CourseGrades.java
+++ Chapter9/src/Mastery/CourseGrades.java
@@ -1,5 +1,33 @@
 package Mastery;
 
-public class CourseGrades {
+import java.util.Scanner;
 
+public class CourseGrades {
+	public static void main(String[] args) {
+		
+		
+		Gradebook studentgrades = new Gradebook();
+		
+		
+		studentgrades.GetGrades();
+		
+		
+		studentgrades.ShowGrades();
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("Please Enter which student (1-12) average you want to see: ");
+		int studentnumber = input.nextInt();
+		System.out.println("Student " + studentnumber + "'s average is:" 
+		+ studentgrades.studentaverage(studentnumber) + "%");
+		
+		
+		System.out.println("Please enter the test (1-5) average you would like to see:");
+		int testnumber =  input.nextInt();
+		System.out.println(" Test number " + testnumber + "'s average is:" 
+				+ studentgrades.testaverage(testnumber) + "%");
+		
+	}
 }
--- Chapter9/src/Mastery/Gradebook.java
+++ Chapter9/src/Mastery/Gradebook.java
@@ -3,17 +3,15 @@ package Mastery;
 import java.util.Scanner;
 
 public class Gradebook {
-
+	
 	private int[][] grades;
 	
-	Scanner input = new Scanner(System.in);
-	
-	public GradeBook() {
+	public Gradebook() {
 		grades = new int[12][5];
-		
 	}
 	
 	public void GetGrades() {
+		Scanner input = new Scanner(System.in);
 		for (int student = 0; student < grades.length; student++){
 			System.out.println("Enter student " + (student +1) + "'s grades:");
 		
@@ -24,12 +22,39 @@ public class Gradebook {
 		}
 		}
 	
-	public void showGrades() {
+	public void ShowGrades() {
 		
 		System.out.println("Gradebook:");
 		for (int student = 0; student < grades.length; student++) {
-			
-		}
+			System.out.println("Student" + (student + 1) + ":");
 		
+		for (int test = 0; test < grades[student].length; test++) {
+			System.out.println(grades[student][test] + "");
+		}
+		System.out.println("");
+		}
 	}
+	
+	public double testaverage(int testnum) {
+		int testi = testnum -1;
+		
+		int sum = 0;
+		
+		for (int student = 0; student < grades.length; student++) {
+			sum+= grades[student][testi];
+		}
+		return (double) sum / grades.length;
+	}
+	
+	public double studentaverage(int studentnum) {
+		int studenti = studentnum -1;
+		
+		int sum = 0;
+		
+		for (int test = 0; test < grades[studenti].length; test++) {
+			sum+= grades[studenti][test];
+		}
+		return (double) sum / grades[studenti].length;
+	}
+	
 }
